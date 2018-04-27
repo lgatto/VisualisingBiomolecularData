da7067067627c33753d03b8826ff239376136880
Laurent <lg390@cam.ac.uk>
2018-04-27 14:35:13 +0100

    add crc msnset

--- src/setup.R
+++ src/setup.R
@@ -1,6 +1,7 @@
 set.seed(1L)
 
 suppressPackageStartupMessages(library("BiocStyle"))
+suppressPackageStartupMessages(library("readr"))
 suppressPackageStartupMessages(library("ggplot2"))
 suppressPackageStartupMessages(library("MSnbase"))
 suppressPackageStartupMessages(library("pRolocdata"))
